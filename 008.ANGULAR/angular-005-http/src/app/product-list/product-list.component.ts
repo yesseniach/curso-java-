0eec8a6119a66f9d758d2c28275bd1303b0ac20c
yesseniach <148784983+yesseniach@users.noreply.github.com>
2024-02-28 20:23:32 +0100

    enrutado

--- 008.ANGULAR/angular-005-http/src/app/product-list/product-list.component.ts
+++ 008.ANGULAR/angular-005-http/src/app/product-list/product-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.models';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+
+  products: Product[] = []; //rellenar este array con productos del backend
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void { //este método se ejecuta automaticamente cuando entras en la pantalla
+   
+    //traer los productos del backend: crea y ejecuta una petición HTTPbcontra un controlador Backend
+    let backend = 'https://fakestoreapi.com/products';
+    this.http.get<Product[]>(backend).subscribe(products => this.products = products);
+
+  }
 
 }
+
