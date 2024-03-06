9f792073840f54ff25599f77d22f271512422b96
yesseniach <148784983+yesseniach@users.noreply.github.com>
2024-03-06 17:52:11 +0100

    angular formulario registr

--- 008.ANGULAR/angular-006-forms/src/app/register/register.component.ts
+++ 008.ANGULAR/angular-006-forms/src/app/register/register.component.ts
@@ -1,12 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Register } from '../model/register.model';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  //no necesita FormBuilder
+  registerForm = new FormGroup({
+    id: new FormControl(''),
+    phone: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  save() {
+    const register: Register = {
+      email: this.registerForm.get('email')?.value ?? '',
+      phone: this.registerForm.get('phone')?.value ?? '',
+      password: this.registerForm.get('password')?.value ?? ''
+    }
+    console.log(register);
+    //enviar registro a backend
+
+    //limpiar el formulario
+    this.registerForm.reset();
+  }
 
 }
